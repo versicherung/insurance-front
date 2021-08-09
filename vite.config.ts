d153a383c0d592e140006da023a23a1a8a6d09f4
wangjq4214 <w1637894214@163.com>
2021-08-09 16:14:24 +0800

    🎉 feat: 初次提交

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [reactRefresh()],
+});
