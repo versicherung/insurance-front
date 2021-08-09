e97c5ba0459b4b310dddbaf4ae64e3da424eb9db
wangjq4214 <w1637894214@163.com>
2021-08-09 18:19:45 +0800

    🔧 perf: vite配置使用函数

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,30 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: /^~/,
-        replacement: path.resolve(__dirname, 'node_modules') + '/',
-      },
-      {
-        find: /@\//,
-        replacement: path.resolve(__dirname, 'src') + '/',
-      },
-    ],
-  },
-  plugins: [reactRefresh()],
-  optimizeDeps: {
-    include: ['@ant-design/icons'],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: [
+        {
+          find: /^~/,
+          replacement: path.resolve(__dirname, 'node_modules') + '/',
+        },
+        {
+          find: /@\//,
+          replacement: path.resolve(__dirname, 'src') + '/',
+        },
+      ],
+    },
+    plugins: [reactRefresh()],
+    optimizeDeps: {
+      include: ['@ant-design/icons'],
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
       },
     },
-  },
+  };
 });
