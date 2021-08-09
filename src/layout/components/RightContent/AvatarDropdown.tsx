b30c33e523ff956099a8b7e39f4e80aaa1b768e9
wangjq4214 <w1637894214@163.com>
2021-08-09 19:22:31 +0800

    ✨ feat: 完善 RightContent

---
+++ src/layout/components/RightContent/AvatarDropdown.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback } from 'react';
+import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Menu, Spin } from 'antd';
+import HeaderDropdown from '../HeaderDropdown';
+
+import styles from './index.module.less';
+
+export type GlobalHeaderRightProps = {
+  menu?: boolean;
+};
+
+const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
+  const initialState = { currentUser: { name: 'Derek', avatar: '' } };
+
+  const onMenuClick = useCallback(() => {
+    console.log('onMenuClick');
+  }, []);
+
+  const loading = (
+    <span className={`${styles.action} ${styles.account}`}>
+      <Spin
+        size="small"
+        style={{
+          marginLeft: 8,
+          marginRight: 8,
+        }}
+      />
+    </span>
+  );
+
+  if (!initialState) {
+    return loading;
+  }
+
+  const { currentUser } = initialState;
+
+  if (!currentUser || !currentUser.name) {
+    return loading;
+  }
+
+  const menuHeaderDropdown = (
+    <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick}>
+      {menu && (
+        <Menu.Item key="center">
+          <UserOutlined />
+          个人中心
+        </Menu.Item>
+      )}
+      {menu && (
+        <Menu.Item key="settings">
+          <SettingOutlined />
+          个人设置
+        </Menu.Item>
+      )}
+      {menu && <Menu.Divider />}
+
+      <Menu.Item key="logout">
+        <LogoutOutlined />
+        退出登录
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <HeaderDropdown overlay={menuHeaderDropdown}>
+      <span className={`${styles.action} ${styles.account}`}>
+        <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
+        <span className={`${styles.name} anticon`}>{currentUser.name}</span>
+      </span>
+    </HeaderDropdown>
+  );
+};
+
+export default AvatarDropdown;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import ProLayout from '@ant-design/pro-layout';
 
 import { Footer } from './components/Footer';
+import RightContent from './components/RightContent';
 
 const LayoutPage: FC = () => {
   return (
@@ -11,6 +12,7 @@ const LayoutPage: FC = () => {
       fixSiderbar
       title="车险出单 Pro"
       footerRender={() => <Footer />}
+      rightContentRender={() => <RightContent />}
     >
       <Outlet />
     </ProLayout>
