89830303dec037b5971035239b9406211a7882f0
wangjq4214 <w1637894214@163.com>
2021-08-09 21:24:44 +0800

    ✨ feat: 添加两个新页面，以便组织路由

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography } from 'antd';
+
+import styles from './Welcome.module.less';
+
+const CodePreview: React.FC = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
+);
+
+const Welcome: FC = () => {
+  return (
+    <PageContainer>
+      <Card>
+        <Alert
+          message="Faster and stronger heavy-duty components have been released."
+          type="success"
+          showIcon
+          banner
+          style={{
+            margin: -12,
+            marginBottom: 24,
+          }}
+        />
+        <Typography.Text strong>
+          {'Advanced Form '}
+          <a
+            href="https://procomponents.ant.design/components/table"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            Welcome
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
+        <Typography.Text
+          strong
+          style={{
+            marginBottom: 12,
+          }}
+        >
+          {'Advanced layout '}
+          <a
+            href="https://procomponents.ant.design/components/layout"
+            rel="noopener noreferrer"
+            target="__blank"
+          >
+            Welcome
+          </a>
+        </Typography.Text>
+        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
+      </Card>
+    </PageContainer>
+  );
+};
+
+export default Welcome;
--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { PartialRouteObject } from 'react-router';
-
 import LayoutPage from '@/layout';
+import Welcome from '@/pages/Welcome';
+import NotFound from '@/pages/404';
+import WarpRoute from './components/WarpRoute';
 
 export const routeList: PartialRouteObject[] = [
   {
     path: '/',
-    element: <LayoutPage />,
+    element: <WarpRoute element={<LayoutPage />} navigate="/welcome" path="/" />,
+    children: [
+      {
+        path: '/welcome',
+        element: <WarpRoute element={<Welcome />} />,
+      },
+      { path: '*', element: <WarpRoute element={<NotFound />} /> },
+    ],
   },
 ];
