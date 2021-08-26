db93975c977c78be08c5b0ba7d290f47a1fdcc3f
wangjq4214 <w1637894214@163.com>
2021-08-26 11:01:03 +0800

    🎨 chore: 删除无用代码补充跳转逻辑

--- config/routes.ts
+++ config/routes.ts
@@ -7,20 +7,6 @@ export default [
       { component: './404' },
     ],
   },
-
-  // { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
-  // {
-  //   path: '/admin',
-  //   name: '管理页',
-  //   icon: 'crown',
-  //   access: 'canAdmin',
-  //   component: './Admin',
-  //   routes: [
-  //     { path: '/admin/sub-page', name: '二级管理页', icon: 'smile', component: './Welcome' },
-  //     { component: './404' },
-  //   ],
-  // },
-  // { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
   {
     name: '订单',
     icon: 'table',
--- src/pages/OrderList/index.tsx
+++ src/pages/OrderList/index.tsx
@@ -1,86 +1,13 @@
 import React, { useState, useRef } from 'react';
+import { history } from 'umi';
 import { Button, message } from 'antd';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
-// import { ModalForm, ProFormText, ProFormTextArea } from '@ant-design/pro-form';
-// import ProDescriptions from '@ant-design/pro-descriptions';
 import { PlusOutlined } from '@ant-design/icons';
 import type { ProFormInstance } from '@ant-design/pro-form';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
-// import type { ProDescriptionsItemProps } from '@ant-design/pro-descriptions';
 
 import { order, exportExcel } from '@/services/api';
-// import type { FormValueType } from './components/UpdateForm';
-// import UpdateForm from './components/UpdateForm';
-/**
- * @en-US Add node
- * @zh-CN 添加节点
- * @param fields
- */
-
-// const handleAdd = async (fields: API.RuleListItem) => {
-//   const hide = message.loading('正在添加');
-
-//   try {
-//     await addRule({ ...fields });
-//     hide();
-//     message.success('Added successfully');
-//     return true;
-//   } catch (error) {
-//     hide();
-//     message.error('Adding failed, please try again!');
-//     return false;
-//   }
-// };
-/**
- * @en-US Update node
- * @zh-CN 更新节点
- *
- * @param fields
- */
-
-// const handleUpdate = async (fields: FormValueType) => {
-//   const hide = message.loading('Configuring');
-
-//   try {
-//     await updateRule({
-//       name: fields.name,
-//       desc: fields.desc,
-//       key: fields.key,
-//     });
-//     hide();
-//     message.success('Configuration is successful');
-//     return true;
-//   } catch (error) {
-//     hide();
-//     message.error('Configuration failed, please try again!');
-//     return false;
-//   }
-// };
-/**
- *  Delete node
- * @zh-CN 删除节点
- *
- * @param selectedRows
- */
-
-// const handleRemove = async (selectedRows: API.RuleListItem[]) => {
-//   const hide = message.loading('正在删除');
-//   if (!selectedRows) return true;
-
-//   try {
-//     await removeRule({
-//       key: selectedRows.map((row) => row.key),
-//     });
-//     hide();
-//     message.success('Deleted successfully and will refresh soon');
-//     return true;
-//   } catch (error) {
-//     hide();
-//     message.error('Delete failed, please try again');
-//     return false;
-//   }
-// };
 
 const handleExportExcel = async (
   formRef: React.MutableRefObject<ProFormInstance | undefined>,
@@ -121,15 +48,8 @@ const handleExportExcel = async (
 };
 
 const TableList: React.FC = () => {
-  // 新建窗口的弹窗
-  // const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-
-  // 分步更新窗口的弹窗
-  // const [updateModalVisible, handleUpdateModalVisible]; = useState<boolean>(false);
-  // const [showDetail, setShowDetail] = useState<boolean>(false);
   const actionRef = useRef<ActionType>();
   const formRef = useRef<ProFormInstance>();
-  // const [currentRow, setCurrentRow] = useState<API.OrderListItem>();
   const [selectedRowsState, setSelectedRows] = useState<API.OrderListItem[]>([]);
 
   const columns: ProColumns<API.OrderListItem>[] = [
@@ -219,7 +139,7 @@ const TableList: React.FC = () => {
             type="primary"
             key="primary"
             onClick={() => {
-              // handleModalVisible(true);
+              history.push('/order/create');
             }}
           >
             <PlusOutlined /> 新建
@@ -260,15 +180,6 @@ const TableList: React.FC = () => {
             </div>
           }
         >
-          {/* <Button
-            onClick={async () => {
-              await handleRemove(selectedRowsState);
-              setSelectedRows([]);
-              actionRef.current?.reloadAndRest?.();
-            }}
-          >
-            批量删除
-          </Button> */}
           <Button
             type="primary"
             onClick={async () => {
@@ -283,82 +194,6 @@ const TableList: React.FC = () => {
           </Button>
         </FooterToolbar>
       )}
-      {/* <ModalForm
-        title={'新建规则'}
-        width="400px"
-        visible={createModalVisible}
-        onVisibleChange={handleModalVisible}
-        onFinish={async (value) => {
-          const success = await handleAdd(value as API.RuleListItem);
-
-          if (success) {
-            handleModalVisible(false);
-
-            if (actionRef.current) {
-              actionRef.current.reload();
-            }
-          }
-        }}
-      >
-        <ProFormText
-          rules={[
-            {
-              required: true,
-              message: '规则名称为必填项',
-            },
-          ]}
-          width="md"
-          name="name"
-        />
-        <ProFormTextArea width="md" name="desc" />
-      </ModalForm>
-      <UpdateForm
-        onSubmit={async (value) => {
-          const success = await handleUpdate(value);
-
-          if (success) {
-            handleUpdateModalVisible(false);
-            setCurrentRow(undefined);
-
-            if (actionRef.current) {
-              actionRef.current.reload();
-            }
-          }
-        }}
-        onCancel={() => {
-          handleUpdateModalVisible(false);
-
-          if (!showDetail) {
-            setCurrentRow(undefined);
-          }
-        }}
-        updateModalVisible={updateModalVisible}
-        values={currentRow || {}}
-      />
-
-      <Drawer 
-        width={600}
-        visible={showDetail}
-        onClose={() => {
-          setCurrentRow(undefined);
-          setShowDetail(false);
-        }}
-        closable={false}
-      >
-        {currentRow?.name && (
-          <ProDescriptions<API.RuleListItem>
-            column={2}
-            title={currentRow?.name}
-            request={async () => ({
-              data: currentRow || {},
-            })}
-            params={{
-              id: currentRow?.name,
-            }}
-            columns={columns as ProDescriptionsItemProps<API.RuleListItem>[]}
-          />
-        )}
-      </Drawer> */}
     </PageContainer>
   );
 };
